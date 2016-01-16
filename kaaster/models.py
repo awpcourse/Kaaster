54aa771a6c6e05f7dd2af0454e0b95807eaeeb68
Razvan Balint <razvannbalint@gmail.com>
2016-01-16 10:35:23 +0200

    Modified models

--- kaaster/models.py
+++ kaaster/models.py
@@ -2,3 +2,18 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.conf import settings
+
+class Post(models.Model):
+
+	author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        )
+
+	date_added = models.DateTimeField(auto_now_add=True)
+	message = models.TextField(max_length=500)
+	media = models.ImageField(upload_to='images/media')
+	tags_ids = [] 
+
+	def __unicode__(self):
+        return u'{} @ {}'.format(self.author, self.date_added)
