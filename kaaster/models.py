f88790e26349a5ef7b6aa5397d663a4d614d332f
IoanaToma <ioana.toma94@yahoo.com>
2016-01-16 11:20:39 +0200

    Minor changes to models

--- kaaster/models.py
+++ kaaster/models.py
@@ -3,20 +3,23 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
+
 class Post(models.Model):
-	author = models.ForeignKey(
+    author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE,
         related_name = 'posts',
         )
+    date_added = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length=500)
+    media = models.ImageField(upload_to='images/media')
 
-	date_added = models.DateTimeField(auto_now_add=True)
-	message = models.TextField(max_length=500)
-	media = models.ImageField(upload_to='images/media')
-	tags_ids = [] 
+    class Meta:
+        ordering = ['-date_added']
 
-	def __unicode__(self):
+    def __unicode__(self):
         return u'{} @ {}'.format(self.author, self.date_added)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -33,25 +36,20 @@ class UserProfile(models.Model):
         default='images/avatars/no_avatar.jpg')
 
     class Meta:
-        ordering = ['date_added']
+        ordering = ['-date_added']
 
     def __unicode__(self):
         return u'{} @ {}'.format(self.first_name, self.last_name)
 
+
 class Tag(models.Model):
-	name = models.TextField(max_length=50)
-	popularity = models.IntegerField()
+    name = models.TextField(max_length=50)
+    popularity = models.IntegerField()
 
-class TagsIntPosts(models.Model):
-	tag = models.ForeignKey(
-		Post,
-		on_delete = models.CASCADE,
-		)
 
-	post = models.ForeignKey(
-		Tag,
-		on_delete = models.CASCADE,
-		)
+class TagsInPosts(models.Model):
+    tag = models.ForeignKey(Post, on_delete = models.CASCADE,)
+    post = models.ForeignKey(Tag, on_delete = models.CASCADE,)
 
 
 class Reply(models.Model):
@@ -69,5 +67,5 @@ class Reply(models.Model):
 
 class TagsInReplies(models.Model):
     reply = models.ForeignKey(Reply, on_delete=models.CASCADE,)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE,)
 
