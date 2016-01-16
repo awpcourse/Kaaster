fd08819ec1c0f012a1ab3af4d7fe782f32965db8
IoanaToma <ioana.toma94@yahoo.com>
2016-01-16 10:28:27 +0200

    UserProfile Model added

--- kaaster/models.py
+++ kaaster/models.py
@@ -2,3 +2,19 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.conf import settings
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        )
+    first_name = models.TextField(max_length=500)
+    last_name = models.TextField(max_length=500)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=1, 
+        choices=(('M', 'Male'), ('F', 'Female')), default='M')
+    avatar = models.ImageField(upload_to='avatars/',
+        default='images/avatars/no_avatar.jpg')
+
+    def __unicode__(self):
+        return u'{} @ {}'.format(self.first_name, self.last_name)
