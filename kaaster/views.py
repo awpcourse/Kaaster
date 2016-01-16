a847e13d30544753052b52f9785f1705d309ce45
theadriann <adriann54@gmail.com>
2016-01-16 10:07:15 +0200

    Basic Things and Updated GitIgnore

--- kaaster/views.py
+++ kaaster/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 
 def index(request):
+    context = {}
     return render(request, 'index.html', context)
